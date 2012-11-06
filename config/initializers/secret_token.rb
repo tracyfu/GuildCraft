eddd676f7409fb3d4916afd28af0e8985009cbd3
Tracy Fu <tracy@foodocs.com>
2012-11-06 01:13:19 -0800

    New Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GuildCraft::Application.config.secret_token = '4aa6ca9cf568f7d24dccc54b0c197020dc115e4edb9220bbbcf3153ad20ce89659dc0096d3782375ab1e8a746b21b72e51181873a8600988932fd94ce22856c4'
