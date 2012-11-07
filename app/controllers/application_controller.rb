ae0746cfb693757fcc7a4aabd3fd4415e556016e
Tracy Fu <tracy@foodocs.com>
2012-11-07 01:25:24 -0800

    Added basic header, dummy Workbench content, and before_filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter { 
+    raise 'Nothing to see here' unless (cookies[:bunnies] || params[:bunnies]) == 'boss'
+    cookies[:bunnies] = 'boss'
+  }
 end
